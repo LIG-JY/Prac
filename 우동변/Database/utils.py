938db8a096c279c82065ee7763f7f07c4bdf8a80
이재연 <dldlsry053@naver.com>
2022-10-18 23:02:24 +0900

    Html&CSS
    10.18 메인 화면 꾸미기(Front-End)

--- 우동변/Database/utils.py
+++ 우동변/Database/utils.py
@@ -9,8 +9,9 @@ client = pymongo.MongoClient(os.environ['MONGO_DB_PATH'])
 UDBdb = client['UDB']  # db_name
 ca_collection = UDBdb['commissioned_area']
 details = deque(ca_collection.find({}))  # CRUD 중 R, Cursor객체(pymongo에서 지원)를 list로 바꿨습니다.
-print(type(details))
-print(details[1])  # class -> dictionary
-print(type(details[0]))
+# print(type(details[3]['city'])) # value's data type is str
+# print(type(details))
+# print(details[1])  # class -> dictionary
+# print(type(details[0]))
 
 
--- 우동변/UDB/views.py
+++ 우동변/UDB/views.py
@@ -5,13 +5,13 @@ from Database.utils import details
 # Create your views here.
 
 
-def index(request):  # request -> HTTP request object
+def index(request):  # request -> HTTP request object, 첫 페이지
     return HttpResponse("안녕하세요 우리동네변호사에 오신것을 환영합니다.")
 
 
 def total(request):  # 전체 표를 보여줍니다.
     return HttpResponse(str(details))
 
-def showManger(request):
+def show(request):  # 목록 조회 코
     total_list = details
 
