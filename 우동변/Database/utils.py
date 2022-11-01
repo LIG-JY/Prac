f8b42d36e3deaf4d7bc4d72806ac7639e18838d2
이재연 <dldlsry053@naver.com>
2022-11-01 23:57:50 +0900

    11.1
    pymongo sql
    html에 파이썬 넘기기
    view.py , urls.py 학습

--- 우동변/Database/utils.py
+++ 우동변/Database/utils.py
@@ -10,8 +10,13 @@ UDBdb = client['UDB']  # db_name
 ca_collection = UDBdb['commissioned_area']
 details = deque(ca_collection.find({}))  # CRUD 중 R, Cursor객체(pymongo에서 지원)를 list로 바꿨습니다.
 # print(type(details[3]['city'])) # value's data type is str
-# print(type(details))
-print(details)  # class -> dictionary
-# print(type(details[0]))
+# print(type(details)) # detail의 data type은 deque
+# print(type(details[0])) # detail각 element의 data type은 딕셔너리
+# print(details)
 
+myquery = {'city': '강원'}
+kangwon = ca_collection.find(myquery)
+kangwon = deque(kangwon)
+for i, v in enumerate(kangwon):
+    print(i, v, type(v))
 
--- 우동변/UDB_app/views.py
+++ 우동변/UDB_app/views.py
@@ -3,29 +3,19 @@ from django.http import HttpResponse
 from Database.utils import details
 from collections import deque
 from django.shortcuts import render
+from Database.utils import kangwon
 
 
 # Create your views here.
 
 
 def index(request):  # request -> HTTP request object, 첫 페이지
-    return render(request, 'home/index.html', {
-                      'form': 'hi'
-                  },
+    return render(request, 'home/index.html',
                   )
-
 
 def total(request):  # 전체 표를 보여줍니다.
     return HttpResponse(str(details))
 
 
-def show_kangwon(request):  # 강원도 목록 조회 코
-    # kangwon = deque()
-    # for detail in details:
-    #     if detail['city'] == '강원':
-    #         kangwon.append(detail)
-    # return kangwon
-    return render(request, 'home/index.html', {
-                      'form': 'hi'
-                  },
-                  )
+def show_kangwon(request):  # 강원도만 보여주게
+    return HttpResponse(kangwon)
